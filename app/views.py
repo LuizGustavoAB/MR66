f9cbe52e5e751a559bce5641347e89f773a8a5a4
Luiz Gustavo <luizgustavo99@live.com>
2022-05-17 19:51:26 -0300

    ap2_pratica

---
+++ app/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render, redirect
+from app.forms import MotosForm
+from app.models import Motos
+from django.core.paginator import Paginator
+
+# Create your views here.
+def home(request):
+    data = {}
+    all = Motos.objects.all()
+    paginator = Paginator(all, 1)
+    pages = request.GET.get('page')
+    data['db'] = paginator.get_page(pages)
+    return render(request, 'index.html', data)
+
+def form(request):
+    data = {}
+    data['form'] = MotosForm()
+    return render(request, 'form.html', data)
+
+def create(request):
+    form = MotosForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('home')
+
+def view(request, pk):
+    data = {}
+    data['db'] = Motos.objects.get(pk=pk)
+    return render(request, 'view.html', data)
+
+def edit(request, pk):
+    data = {}
+    data['db'] = Motos.objects.get(pk=pk)
+    data['form'] = MotosForm(instance=data['db'])
+    return render(request, 'form.html', data)
+
+def update(request, pk):
+    data = {}
+    data['db'] = Motos.objects.get(pk=pk)
+    form = MotosForm(request.POST or None, instance=data['db'])
+    if form.is_valid():
+        form.save()
+        return redirect('home')
+
+def delete(request, pk):
+    db = Motos.objects.get(pk=pk)
+    db.delete()
+    return redirect('home')
